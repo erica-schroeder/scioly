3ac742856267917236e16d3f35c67b32958afbee
Erica Schroeder <schroeder.erica.m@gmail.com>
2026-03-15 13:00:48 -0400

    Updates

---
+++ assets/odonata-C2F_wNeA.js
@@ -0,0 +1,30 @@
+const e=[{question:"What order/family is this insect in?",answer:`Odonata
+Aeshnidae (darners)
+
+-Chunky
+-long body`,image:"aeshnidae-immature.png"},{question:"What order/family is this insect in?",answer:`Odonata
+Aeshnidae (darners)
+
+-Large
+-Long abdomen
+-Eyes barely separated
+-Blue, green, or brown`,image:"aeshnidae.jpg"},{question:"What order/family is this insect in?",answer:`Odonata
+Libellulidae (skimmers)
+
+Chunky abdomen, eyes barely touch`,image:"libellulidae.jpg"},{question:"What order/family is this insect in?",answer:`Odonata
+Lestidae (spread-wing damselflies)
+
+-Petite
+-Super long abdomen`,image:"lestidae-immature.jpg"},{question:"What order/family is this insect in?",answer:`Odonata
+Coenagrionidae (narrow-wing damselflies)
+
+-Petite
+-3 leaf-like gills on tail`,image:"coenagrionidae-immature.jpg"},{question:"What order/family is this insect in?",answer:`Odonata
+Libellulidae (skimmers)
+
+-Chunky
+-Short, rounder body`,image:"libellulidae-immature.jpg"},{question:"What order/family is this insect in?",answer:`Odonata
+Lestidae (spread-wing damselflies)
+
+-Wings open at rest
+-Less brightly colored`,image:"lestidae.jpg"}];export{e as default};
---
+++ assets/orthoptera-BRuo3Il1.js
@@ -0,0 +1,22 @@
+const e=[{question:"What order/family is this insect in?",answer:`Orthoptera
+Acrididae
+(short-horned grasshoppers)
+
+-Antennae shorter than body`,image:"acrididae.jpg"},{question:"What order/family is this insect in?",answer:`Orthoptera
+Gryllotalpidae
+(mole crickets)
+
+-Fossorial legs for digging`,image:"raphidophoridae.jpg"},{question:"What order/family is this insect in?",answer:`Orthoptera
+Gryllidae
+(crickets & tree crickets)
+
+-Tree cricket`,image:"tree-cricket.jpg"},{question:"What order/family is this insect in?",answer:`Orthoptera
+Tettigonidae
+(katydids)
+
+-Ridiculously long hindlegs
+-Long, thin antennae`,image:"brown-katydid.jpg"},{question:"What order/family is this insect in?",answer:`Orthoptera
+Gryllidae
+(crickets & tree crickets)
+
+-Regular cricket`,image:"cricket.jpg"}];export{e as default};
