43f45ae06156aabd27e2dbd5d892f15589360ccf
Erica Schroeder <schroeder.erica.m@gmail.com>
2026-03-15 12:24:47 -0400

    Updates

---
+++ assets/general-questions-CwUTy1GJ.js
@@ -0,0 +1,1 @@
+const e=[{question:"How do black and yellow garden spiders benefit humans?",answer:"They eat grasshoppers that are garden/agricultural pests"},{question:"Do black and yellow garden spiders spin webs?",answer:"Yes"}];export{e as default};
