48fe697723e4d8e316636b9e7ab027a326c9ae0f
Erica Schroeder <schroeder.erica.m@gmail.com>
2026-04-15 19:15:05 -0400

    Updates

---
+++ assets/hemiptera-jHsxgCwn.js
@@ -0,0 +1,32 @@
+const e=[{question:"What order/family is this insect in?",answer:`Hemiptera
+Corixidae (water boatmen)`,image:"water-boatman.jpg"},{question:"What order/family is this insect in?",answer:`Hemiptera
+Notonectidae (backswimmers)`,image:"backswimmer.jpg"},{question:"What order/family is this insect in?",answer:`Hemiptera
+Belostomatidae (giant water bugs)`,image:"giant-water-bug.jpg"},{question:"What order/family is this insect in?",answer:`Hemiptera
+Nepidae (waterscorpions)`,image:"water-scorpion.jpg"},{question:"What order/family is this insect in?",answer:`Hemiptera
+Gelastocoridae (toad bugs)`,image:"toad-bug.jpg"},{question:"What order/family is this insect in?",answer:`Hemiptera
+Gerridae (water striders)`,image:"water-strider.jpg"},{question:"What order/family is this insect in?",answer:`Hemiptera
+Cimicidae (bed bugs)`,image:"bed-bug.jpg"},{question:"What order/family is this insect in?",answer:`Hemiptera
+Miridae (plant bugs)
+
+-Triangular section at the tip of the wings (cuneus)`,image:"plant-bug.jpg"},{question:"What order/family is this insect in?",answer:`Hemiptera
+Reduviidae (assassin bugs)`,image:"assassin-bug.jpg"},{question:"What order/family is this insect in?",answer:`Hemiptera
+Scutelleridae (metallic shield bugs)`,image:"metallic-shield-bug.jpg"},{question:"What order/family is this insect in?",answer:`Hemiptera
+Tingidae (lace bugs)`,image:"lace-bug.jpg"},{question:"What order/family is this insect in?",answer:`Hemiptera
+Tingidae (lace bugs)`,image:"lace-bug2.jpg"},{question:"What order/family is this insect in?",answer:`Hemiptera
+Lygaeidae (seed bugs)`,image:"seed-bug.jpg"},{question:"What order/family is this insect in?",answer:`Hemiptera
+Coreidae (leaf-footed bugs)`,image:"coreidae.jpg"},{question:"What order/family is this insect in?",answer:`Hemiptera
+Pentatomidae (stink bugs)`,image:"pentatomidae.jpg"},{question:"What order/family is this insect in?",answer:`Hemiptera
+Cicadidae (cicadas)`,image:"cicadidae.jpg"},{question:"What order/family is this insect in?",answer:`Hemiptera
+Cicadidae (cicadas)`,image:"cicada-nymph.jpg"},{question:"What order/family is this insect in?",answer:`Hemiptera
+Membracidae (treehoppers)`,image:"membracidae.jpg"},{question:"What order/family is this insect in?",answer:`Hemiptera
+Cercopidae (froghoppers, spittlebugs)`,image:"cercopidae.jpg"},{question:"What order/family is this insect in?",answer:`Hemiptera
+Cicadellidae (leafhoppers)`,image:"cicadellidae.jpg"},{question:"What order/family is this insect in?",answer:`Hemiptera
+Fulgoridae (folgorid planthoppers)`,image:"fulgoridae.jpg"},{question:"What order/family is this insect in?",answer:`Hemiptera
+Aphididae (aphids)`,image:"aphididae.jpg"},{question:"What order/family is this insect in?",answer:`Hemiptera
+Pseudococcidae (mealybugs)
+
+-Female`,image:"pseudococcidae.jpg"},{question:"What order/family is this insect in?",answer:`Hemiptera
+Pseudococcidae (mealybugs)
+
+-Male`,image:"pseudococcidae-male.jpg"},{question:"What order/family is this insect in?",answer:`Hemiptera
+Coccidae (soft scale insect)`,image:"coccidae.jpg"}];export{e as default};
